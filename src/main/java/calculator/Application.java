3282f15cca6457a3d76ac64dbb76470db4140285
An-Haeeun-02 <godms531@naver.com>
2025-10-20 03:21:18 +0900

    feat(custom-delimiter): add custom delimiter check and extraction
    why:
    - 사용자가 지정한 커스텀 구분자를 처리하기 위한 기능 추가
    - 구분자 양식 검증, 문자열에서 해당 구분자 추출
    
    what/how:
    - 커스텀 구분자가 있는지 확인, 구분자 양식 검증
    - 커스텀 구분자 추출
    - 문자열에서 커스텀 양식을 삭제 후 정제 데이터 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,9 +7,11 @@ import java.util.regex.Pattern;
 public class Application {
 
     // 기본 구분자 지정
-    private static String DELIMITER = "[,;]";
+    private static final String DELIMITER = "[,;]";
     //커스텀 구분자 패턴
     private static final Pattern CUSTOM_PATTERN_DELIMITER = Pattern.compile("//(.+)\n(.*)");
+    //커스텀 구분자 지정
+    private static String CUSTOM_DELIMITER ="";
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -41,23 +43,47 @@ public class Application {
             throw new IllegalArgumentException();
         }
 
-
         //첫번째 문자가 숫자가 아닌경우
         char firstChar = input.charAt(0);
         if (!Character.isDigit(firstChar)) { //숫자가 아니라면
             if (firstChar == ',' || firstChar == ';') { //,나 ; 라면
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("입력이 구분자로 시작합니다.");
+            } else {
+                //커스텀 문자열 시도로 추청. 점검 코드 시
+                input = customCheck(input);
             }
-            try {
-                //숫자도 아니고, 구분자도 아니라면 커스텀 문자열 점검 함수 예정
-
-            }catch (IllegalArgumentException e){
-                throw new IllegalArgumentException();
-            }
-
         }
-
+        //입력값 반환
         return input;
     }
+
+    //커스텀 구분자 여부 점검
+    private static String customCheck(String input) throws IllegalArgumentException {
+        //커스텀 문자열 패턴
+        Matcher matcher = CUSTOM_PATTERN_DELIMITER.matcher(input);
+        //빈 문자열 점검
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("빈 문자열 입니다.");
+        }
+
+        //커스텀 패턴 일지/존재 여부
+        if(matcher.matches()){//커스텀 패턴과 일치하면
+            // 지정된 커스텀 문자열 추출
+            String customDelimiter = matcher.group(1);
+            //커스텀 문자열을 저장
+            CUSTOM_DELIMITER = customDelimiter;
+
+            //커스텀 부분을 제외한 뒷부분만 추출
+            String modifiedInput = matcher.group(2);
+            //수정된 문자열로 수정
+            input = modifiedInput;
+        } else {
+            throw new IllegalArgumentException("커스텀 패턴에 오류가 있습니다.");
+        }
+
+        //커스텀 부분을 제외한, 가공괸 문자열을 반환함
+        return new String(input.getBytes());
+    }
+
 
 }
